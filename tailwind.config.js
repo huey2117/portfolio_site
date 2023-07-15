7d0a28b7292796f2f80db729a387469f7f035108
Michael Huey <michaelyeuh@gmail.com>
2023-07-15 00:17:00 -0400

    feat(rebuild): minor styling updates and windows installs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
-import defaultTheme from 'tailwindcss/defaultTheme'
-import forms from '@tailwindcss/forms'
-import typography from '@tailwindcss/typography'
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+import typography from '@tailwindcss/typography';
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -15,7 +15,15 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Spectral', ...defaultTheme.fontFamily.sans],
+        sans: ['Roboto Condensed', 'sans-serif'],
+      },
+      colors: {
+        'primary-light': '#4F46E5',
+        'primary-dark': '#C4B5FD',
+        'secondary-light': '#10B981',
+        'secondary-dark': '#065F46',
+        'tertiary-light': '#3B82F6',
+        'tertiary-dark': '#1D4ED8',
       },
     },
   },
